b9a446fa09b0ddca81902cded36645901c4718f2
012NaveenG <012guptagkp@gmail.com>
2024-12-27 12:57:48 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(
   express.json({
     limit: "16kb",
@@ -23,16 +18,12 @@ app.use(
 app.use(cookieParser());
 app.use(express.static("public"));
 
-
-
-
-
 //admin routes
 import adminEmpRoutes from "./admin/routes/employee.routes.js";
 import adminCustomerRoutes from "./admin/routes/customer.routes.js";
 import admintruckRoutes from "./admin/routes/truck.routes.js";
 import adminSalesRoutes from "./admin/routes/sales.routes.js";
-import adminOverviewRoutes from "./admin/routes/overview_dashboard.routes.js"
+import adminOverviewRoutes from "./admin/routes/overview_dashboard.routes.js";
 
 app.use("/api/admin/employee", adminEmpRoutes);
 app.use("/api/admin/customer", adminCustomerRoutes);
@@ -40,9 +31,8 @@ app.use("/api/admin/truck", admintruckRoutes);
 app.use("/api/admin/sales", adminSalesRoutes);
 app.use("/api/admin/overview", adminOverviewRoutes);
 
-
 //Employee routes
-import employeeRoutes from "./routes/employee.routes.js"
-app.use("/api/employee",employeeRoutes)
+import employeeRoutes from "./routes/employee.routes.js";
+app.use("/api/employee", employeeRoutes);
 
 export { app };
